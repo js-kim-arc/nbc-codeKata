28f56ff6edc886a9658bc0185c68e74a4191c42e
junseong kim <rlawnstj90@naver.com>
2026-05-28 09:45:30 +0900

    30

---
+++ coding/30.py
@@ -0,0 +1,6 @@
+def solution(a, b):
+    days_in_month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    week = ["FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU"]
+
+    elapsed = sum(days_in_month[:a - 1]) + (b - 1)
+    return week[elapsed % 7]
